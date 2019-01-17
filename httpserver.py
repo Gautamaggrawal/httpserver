4f73997a2951b603647b35afb7ad1aaefc8541c0
Gautam Aggrawal <gautamaggrawalmzn@gmail.com>
2019-01-17 17:44:28 +0530

    sd

--- httpserver.py
+++ httpserver.py
@@ -34,6 +34,32 @@ class RestHandler(BaseHTTPRequestHandler):
                 self.end_headers()
         except:
             pass
+    # def do_GET(self):
+    #     try:
+    #         if None != re.search('/api/get/\d', self.path):
+    #             recordID =int(self.path.split('/')[-1])
+    #             if records.has_key(recordID):
+    #                 self.send_response(200)
+    #                 self.send_header('Content-type','application/json')
+    #                 self.end_headers()
+    #                 print(records[recordID])
+    #                 self.wfile.write(json.dumps(records[recordID]))
+    #             else:
+    #                 self.send_response(400, 'Bad Request: record does not exist')
+    #                 self.send_header('Content-Type', 'application/json')
+    #                 self.end_headers()
+    #                 self.wfile.write(json.dumps({'error': {'message': 'Bad Request: record does not exist', 'code': 400, 'type': 'Exception'}}))
+    #         else:
+    #             self.send_response(403)
+    #             self.send_header('Content-Type', 'application/json')
+    #             self.end_headers()
+    #             self.wfile.write(json.dumps({'error': {'message': 'Unsupported GET request', 'code': 403, 'type': 'Exception'}}))
+
+    #         return
+
+    #     except IOError:
+    #         self.send_error(404, 'File Not Found: %s' % self.path)
+
     def do_GET(self):
         try:
             if None != re.search('/api/get/\d', self.path):
@@ -57,11 +83,6 @@ class RestHandler(BaseHTTPRequestHandler):
 
             return
 
-        except IOError:
-            self.send_error(404, 'File Not Found: %s' % self.path)
-
-    def do_GET(self):
-        try:
             if None != re.search('/api/list/', self.path):
                 if len(records)!=0:
                     self.send_response(200)
